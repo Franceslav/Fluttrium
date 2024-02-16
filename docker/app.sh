ed4da5b2e920aaa3b3b768463727d1d70cd3be01
Franceslav <107437303+Franceslav@users.noreply.github.com>
2024-02-16 09:38:24 +0300

    Update app.sh

--- docker/app.sh
+++ docker/app.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-while !</dev/tcp/db/5432;
-do sleep 1;
-done;
+# Ожидание доступности базы данных
+echo "Ожидание доступности базы данных на $DB_HOST:$DB_PORT..."
+while ! nc -z $DB_HOST $DB_PORT; do
+  sleep 1
+done
+echo "База данных доступна."
+
+# Применение миграций Alembic
+echo "Применение миграций Alembic..."
 alembic upgrade head
 
-gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+# Запуск приложения FastAPI через Gunicorn
+echo "Запуск FastAPI приложения через Gunicorn..."
+exec gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
