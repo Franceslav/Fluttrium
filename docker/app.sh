62ecc051cc9c9b613838cd7ba9781b14d181e5df
Franceslav <107437303+Franceslav@users.noreply.github.com>
2024-02-16 11:44:37 +0300

    Update app.sh

--- docker/app.sh
+++ docker/app.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Экспорт переменных окружения для подключения к базе данных
-export DB_HOST=dpg-cn781dg21fec73fkca90-a
-export DB_PORT=5431
-export DB_NAME=postgres_test_fcb1
-export DB_USER=postgres_test
-export DB_PASS=VR8KrGocgPOkgW8fYrLFTowU7kkDOjDN
+# Определение переменных среды для подключения к базе данных
+export DB_HOST="dpg-cn781dg21fec73fkca90-a.frankfurt-postgres.render.com"
+export DB_PORT=5432
+export DB_NAME="postgres_test_fcb1"
+export DB_USER="postgres_test"
+export DB_PASS="VR8KrGocgPOkgW8fYrLFTowU7kkDOjDN"
 
 # Ожидание доступности базы данных
 echo "Ожидание доступности базы данных на $DB_HOST:$DB_PORT..."
@@ -14,10 +14,10 @@ while ! nc -z $DB_HOST $DB_PORT; do
 done
 echo "База данных доступна."
 
-# Применение миграций Alembic
+# Применение миграций Alembic (если требуется)
 echo "Применение миграций Alembic..."
 alembic upgrade head
 
 # Запуск приложения FastAPI через Gunicorn
 echo "Запуск FastAPI приложения через Gunicorn..."
-gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+exec gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
